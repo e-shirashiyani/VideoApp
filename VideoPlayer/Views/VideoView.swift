9e5ccc6e6079e1fd20e4307b2faeaf7ccbd3f637
e.shirashiyani <e.shirashiyani>
2022-02-15 11:39:27 +0330

    add toggle behind of video

---
+++ VideoPlayer/Views/VideoView.swift
@@ -0,0 +1,32 @@
+//
+//  VideoView.swift
+//  VideoPlayer
+//
+//  Created by e.shirashiyani on 2/11/22.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoView: View {
+    var video : Video
+    @State private var player = AVPlayer()
+    var body: some View {
+        
+        VideoPlayer(player:player)
+            .edgesIgnoringSafeArea(.all)
+            .onAppear{
+                if let link=video.videoFiles.first?.link{
+                    player=AVPlayer(url: URL(string: link)!)
+                    player.play()
+                }
+            }
+    }
+    
+}
+
+struct VideoView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoView(video: previewVideo)
+    }
+}
